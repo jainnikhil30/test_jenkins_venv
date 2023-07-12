0dc31b1b770431ab66feb50e7c8449c039d578f3
jainnikhil30 <jainnikhil30@gmail.com>
2023-07-12 18:41:15 +0530

    Create venv.groovy

---
+++ venv.groovy
@@ -0,0 +1,36 @@
+#!groovy
+
+@Library('insights-cpt-ci-configs') _
+
+// Create MARKER variable that will be common for all sub-builds
+def MARKER = """${ new Date().format("yyyy-MM-dd'T'HH:mm:ss").toString().replaceAll('[^a-zA-Z0-9-]', '_') }"""
+def STATUS_DATA_FILE = """status-data-${MARKER}.json"""
+
+
+pipeline {
+    agent {
+        kubernetes {
+            inheritFrom "cpt-v2"
+            defaultContainer "compute"
+            label "cpt-v2"
+        }
+    }
+
+    environment {
+        MARKER = "${MARKER}"
+        STATUS_DATA_FILE = "${STATUS_DATA_FILE}"
+    }
+
+    stages {
+        stage('Setup') {
+            steps {
+               script {
+                        globalHelpers.helperCreateVenv()
+                    }
+            }
+            steps {
+                sleep(time: 10, unit: 'MINUTES')
+            }
+        }
+    }
+}
